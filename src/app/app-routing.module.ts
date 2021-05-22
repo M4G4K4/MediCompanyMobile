2bdceb9835dfe97c524b1b3a93c9b6e5fc364ef7
Pedro Dias <pedro158a@gmail.com>
2021-05-22 14:44:28 +0100

    Add register screen
    Signed-off-by: Pedro Dias <pedro158a@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,11 @@ const routes: Routes = [
     path: '',
     redirectTo: '/login',
     pathMatch: 'full'
+  },  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
+
 ];
 @NgModule({
   imports: [
