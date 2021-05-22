7b0d47dbab9449622a946667bceb5501601c4dd5
Pedro Dias <pedro158a@gmail.com>
2021-05-22 14:10:04 +0100

    Add register screen
    Signed-off-by: Pedro Dias <pedro158a@gmail.com>

---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  credentials: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register() {
+
+  }
+}
